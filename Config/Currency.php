12144ead2d63f6bc1efd3c482cf685d61284a267
luigi <luigiambra@dnafactory.it>
2021-05-21 09:57:42 +0200

    + Implementata la gestione delle currency tramite di.xml + Aggiornato readme

--- Config/Currency.php
+++ Config/Currency.php
@@ -11,19 +11,31 @@ class Currency
      * @var \Magento\Framework\App\Config\ScopeConfigInterface
      */
     protected $_scopeConfig;
+    /**
+     * @var array
+     */
+    protected $currenciesPositions;
 
-    public function __construct(ScopeConfigInterface $scopeConfig)
-    {
+    public function __construct (
+        ScopeConfigInterface $scopeConfig,
+        array $currenciesPositions = []
+    ) {
         $this->_scopeConfig = $scopeConfig;
+        $this->currenciesPositions = $currenciesPositions;
     }
 
     public function getPositionByBaseCode($baseCode)
     {
-        $position = (int)$this->_scopeConfig->getValue('dnafactory/currency/position', ScopeInterface::SCOPE_STORE);
-        if($position == '') {
-            $position = \Magento\Framework\Currency::LEFT;
+        $baseCode = trim($baseCode);
+
+        $defaultPosition = (int)$this->_scopeConfig->getValue('dnafactory/currency/position', ScopeInterface::SCOPE_STORE);
+        if($defaultPosition == '') {
+            $defaultPosition = \Magento\Framework\Currency::LEFT;
         }
-        $positions = ['EUR' => $position];
-        return $positions[$baseCode];
+
+        if (!array_key_exists($baseCode, $this->currenciesPositions)) {
+            return $defaultPosition;
+        }
+        return $this->currenciesPositions[$baseCode];
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Currency
+#Currency
+
+Con questo modulo le posizioni delle valute sono gestibili da di.xml come segue:
+
+```xml
+<type name="DNAFactory\Currency\Config\Currency">
+    <arguments>
+        <argument name="currenciesPositions" xsi:type="array">
+            <item name="EUR" xsi:type="const">\Magento\Framework\Currency::RIGHT</item>
+            <item name="USD" xsi:type="const">\Magento\Framework\Currency::LEFT</item>
+        </argument>
+    </arguments>
+</type>
+```
+
+Esiste la configurazione dnafactory/currency/position che determina una posizione di default per tutte le currency non definite in di
